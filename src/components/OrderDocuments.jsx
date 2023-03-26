9445dcf3a983071a3bda9f92ce63988417f838fa
christian-sugianto <christianhadinata.sugi@gmail.com>
2023-03-27 10:23:36 +1100

    feat: WIP

--- src/components/OrderDocuments.jsx
+++ src/components/OrderDocuments.jsx
@@ -103,36 +103,9 @@ class OrderDocuments extends React.Component {
     return shouldProceed;
   };
 
-  handleDropdownChange = async ({ fieldName, value, label }) => {
-    const type = fieldName === 'locale' ? this.state.type.value : value
-    const locale = fieldName === 'locale' ? value : this.state.locale.value;
-    
-    if (type === '') {
-      this.setState({ [fieldName]: { value, label } }, () => {
-        this.getFields();
-      });
-      return;
-    }
-
-    let count;
-    let documents;
-
-    if (type === 'promotionFresh') {
-      count = await client.fetch(
-        `count(*[!(_id in path("drafts.**")) && _type == $types && ("${locale}" in languageList || "all" in languageList) && (__i18n_lang == 'en' || __i18n_lang == "${locale}")])`, 
-        { types: type }
-      );
-      documents = await client.fetch(
-        `*[!(_id in path("drafts.**")) && _type == $types && ("${locale}" in languageList || "all" in languageList) && (__i18n_lang == 'en' || __i18n_lang == "${locale}")] | order (${this.state.field.value} asc, order asc, _updatedAt desc)[0...${PAGE_SIZE}]`,
-        { types: type }
-      );
-
-      const filteredDocumentIds = documents.map(document => document.__i18n_base?._ref).filter(document => document !== undefined)
-      documents = documents.filter(document => !filteredDocumentIds.includes(document._id))
-    } else {
-      count = await client.fetch(`count(*[!(_id in path("drafts.**")) && _type == $types && __i18n_lang == "en"])`, { types: value, }); 
-      documents = await client.fetch( `*[!(_id in path("drafts.**")) && _type == $types && __i18n_lang == "en"] | order (${this.state.field.value} asc, order asc, _updatedAt desc)[0...${PAGE_SIZE}]`, { types: value } );
-    }
+  handleTypeChange = async ({ value, label }) => {
+    const count = await client.fetch(`count(*[!(_id in path("drafts.**")) && _type == $types])`, { types: value, }); 
+    const documents = await client.fetch( `*[!(_id in path("drafts.**")) && _type == $types] | order (${this.state.field.value} asc, order asc, _updatedAt desc)[0...${PAGE_SIZE}]`, { types: value } );
 
     const shouldProceed = this.isSafeToProceed(documents, this.state.field, { value, label });
 
@@ -158,7 +131,7 @@ class OrderDocuments extends React.Component {
         }
       }
 
-      this.setState({ [fieldName]: { value, label }, documents, count }, () => {
+      this.setState({ type: { value, label }, documents, count }, () => {
         this.getFields();
       });
 
@@ -167,6 +140,12 @@ class OrderDocuments extends React.Component {
       }
     }
   };
+
+  handleLocaleChange = ({ value, label }) => {
+    this.setState({ locale: { value, label } }, () => {
+      this.getFields();
+    });
+  }
 
   handleFieldChange = async ({ value, label }) => {
     const count = await client.fetch(`count(*[!(_id in path("drafts.**")) && _type == $types])`, {
@@ -213,7 +192,8 @@ class OrderDocuments extends React.Component {
             <div className={styles.orderDocumentsInnerWrapper}>
               <TypeSection
                 {...this.state}
-                handleDropdownChange={this.handleDropdownChange}
+                handleTypeChange={this.handleTypeChange}
+                handleLocaleChange={this.handleLocaleChange}
                 handleFieldChange={this.handleFieldChange}
                 refreshTypes={this.refreshTypes}
               />
